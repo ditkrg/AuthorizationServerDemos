c48002d025d253b27c51b2f72df99b0a694c47b1
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-13 19:12:31 +0300

    Creates Navbar component

---
+++ React/src/pages/components/VehicleRegisterForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VehicleRegisterForm = ()=> (
+    <form className="w-64 flex flex-col justify-center shadow">
+        <h3>Register A Vehicle</h3>
+    </form>
+)
+
+export default VehicleRegisterForm
