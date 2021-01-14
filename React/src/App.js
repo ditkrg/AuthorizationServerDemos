88612e9cff6db84f3693a24e9f114462c626d740
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-14 11:56:38 +0300

    Fixes availablity of user for Home component

--- React/src/App.js
+++ React/src/App.js
@@ -10,12 +10,27 @@ import userManager, { loadUserFromStorage } from './services/userService';
 import AuthProvider from './utils/authProvider';
 import PrivateRoute from './utils/protectedRoute';
 import './index.css';
+import {storeUser} from './actions/authActions'
 
 function App() {
   useEffect(() => {
+    // Commented out not to check the localStorage for the user's existence ** Temporary **
+
     // fetch current user from cookies
-    loadUserFromStorage(store);
+    // loadUserFromStorage(store);
   }, []);
+
+ 
+
+  // Should be removed ** Temporary **
+  const logUserIn = () => {
+    const user = {profile: {given_name: 'John Doe'}}
+    console.log(`User logged in!`)
+    store.dispatch(storeUser(user))
+  }
+
+   // ** Temporary **
+   logUserIn()
 
   return (
     <Provider store={store}>
--- React/src/pages/components/LoginWindow.js
+++ React/src/pages/components/LoginWindow.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Button from './Button';
+import {storeUser} from '../../actions/authActions'
+import store from '../../store'
+import userManager from '../../services/userService'
 
 const LoginWindow = () => {
+
+
+  const onUserLogin = () => {
+    const user = {profile: {given_name: 'John Doe'}}
+    console.log(`User logged in!`)
+    store.dispatch(storeUser(user))
+    // Looks like we don't have this function even tho it's in the library installed
+    // userManager.storeUser(user).then(()=> console.log('yay')).catch(()=> console.log('nope'))
+  }
+
   return (
     <div className="xl:w-1/3 lg:w-5/12 md:w-2/4 sm:w-9/12 w-11/12 flex flex-col justify-center shadow md:px-32 sm:px-28 md:py-40 sm:py-36 px-20 py-16">
       <h1 className="font-playfair text-black-custom text-6xl mb-12">
@@ -10,7 +23,7 @@ const LoginWindow = () => {
       <p className="text-black-custom font-light text-3xl mb-20">
         Login to your account to view your dashboard and register a new vehicle.
       </p>
-      <Button />
+      <Button onClickEvent={onUserLogin} />
     </div>
   );
 };
