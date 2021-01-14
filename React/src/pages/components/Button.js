4d6c0fd1d2010eaf3dfd08f9cc9cc4482c589d73
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-14 11:56:38 +0300

    Fixes availablity of user for Home component

--- React/src/pages/components/Button.js
+++ React/src/pages/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = () => (
-  <button className="bg-blue-custom border-0 font-inter font-semibold rounded text-xl m-0">
+const Button = ({onClickEvent}) => (
+  <button onClick={onClickEvent} className="bg-blue-custom border-0 font-inter font-semibold rounded text-xl m-0">
     Login
   </button>
 );
