326ed625648c71b785ea851013a15d4310cef364
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-14 06:20:00 +0300

    Changes file extension

--- React/src/pages/components/VehicleRegisterForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VehicleRegisterForm = ()=> (
-    <form className="w-64 flex flex-col justify-center shadow">
-        <h3>Register A Vehicle</h3>
-    </form>
-)
-
-export default VehicleRegisterForm
