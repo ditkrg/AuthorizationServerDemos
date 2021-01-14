ce5b712bd2230cc4caccd4cc42338536abb6470f
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-14 05:59:46 +0300

    Modifies Navbar to add links

--- React/src/App.js
+++ React/src/App.js
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import SigninOidc from './pages/signin-oidc'
-import SignoutOidc from './pages/signout-oidc'
-import Home from './pages/home'
-import Login from './pages/login'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SigninOidc from './pages/signin-oidc';
+import SignoutOidc from './pages/signout-oidc';
+import Home from './pages/home';
+import Login from './pages/login';
 import { Provider } from 'react-redux';
 import store from './store';
-import userManager, { loadUserFromStorage } from './services/userService'
-import AuthProvider from './utils/authProvider'
-import PrivateRoute from './utils/protectedRoute'
+import userManager, { loadUserFromStorage } from './services/userService';
+import AuthProvider from './utils/authProvider';
+import PrivateRoute from './utils/protectedRoute';
+import './index.css';
 
 function App() {
-
   useEffect(() => {
     // fetch current user from cookies
-    loadUserFromStorage(store)
-  }, [])
+    loadUserFromStorage(store);
+  }, []);
 
   return (
     <Provider store={store}>
--- React/src/pages/components/Navbar.jsx
+++ React/src/pages/components/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import logo from '../../images/logo.png'
+import React from 'react';
+import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Navbar = ()=>(
-    <div className="w-100 bg-blue-400 flex justify-center items-center" style={{height: "90px"}}>
-        <div className="flex items-center w-4/5">
-            <img src={logo} alt="logo" className="h-28"/>
-        </div>
+const Navbar = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  return (
+    <div
+      className="w-100 bg-blue-custom flex justify-center items-center"
+      style={{ height: '90px' }}
+    >
+      <nav className="flex items-center justify-between w-4/5">
+        <img src={logo} alt="logo" className="h-28" />
+        {user ? (
+          <ul className="list-none flex items-center justify-between w-1/5 m-0">
+            <li className="m-0">
+              <Link
+                to="/vehicles"
+                className="text-white font-inter text-xl font-semibold hover:text-white"
+              >
+                Your Vehicles
+              </Link>
+            </li>
+            <li className="m-0">
+              <Link
+                to="/register"
+                className="text-white font-inter text-xl font-semibold hover:text-white"
+              >
+                Register A Vehicles
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          ''
+        )}
+      </nav>
     </div>
-)
-export default Navbar
+  );
+};
+export default Navbar;
