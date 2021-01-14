64818b50958723dbd87fdcb7bd1a0a47e1599508
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-14 07:33:17 +0300

    Login page done and responsive

---
+++ React/src/pages/components/LoginWindow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './Button';
+
+const LoginWindow = () => {
+  return (
+    <div className="xl:w-1/3 lg:w-5/12 md:w-2/4 sm:w-9/12 w-11/12 flex flex-col justify-center shadow md:px-32 sm:px-28 md:py-40 sm:py-36 px-20 py-16">
+      <h1 className="font-playfair text-black-custom text-6xl mb-12">
+        Traffic Police Service
+      </h1>
+      <p className="text-black-custom font-light text-3xl mb-20">
+        Login to your account to view your dashboard and register a new vehicle.
+      </p>
+      <Button />
+    </div>
+  );
+};
+
+export default LoginWindow;
