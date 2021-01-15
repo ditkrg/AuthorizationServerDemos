19d09285890c77ce477f4ec2533f489f12ebd4b3
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-01-15 19:05:30 +0300

    Makes Navbar responsive

--- React/src/pages/components/Navbar.js
+++ React/src/pages/components/Navbar.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../images/logo.svg";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { IoMdMenu } from "react-icons/io";
+import { useMediaQuery } from "react-responsive";
+
+// Services
 import { signoutRedirect } from "../../services/userService";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ query: "(max-width: 950px)" });
+
   const user = useSelector((state) => state.auth.user);
 
   function signOut() {
@@ -16,38 +23,57 @@ const Navbar = () => {
       className="w-screen bg-blue-custom flex justify-center items-center fixed top-0 left-0"
       style={{ height: "80px" }}
     >
-      <nav className="flex items-center justify-between w-4/5">
+      <nav className="flex items-center justify-between xl:w-4/5 w-10/12">
         <Link to="/">
           <img src={logo} alt="logo" className="h-24" />
         </Link>
         {user ? (
-          <ul className="list-none flex items-center justify-between w-2/6 m-0">
-            <li className="m-0">
-              <Link
-                to="/"
-                className="text-white font-inter font-semibold hover:text-white focus:text-white tracking-normal"
-                style={{ fontSize: "1.36rem" }}
-              >
-                Your Vehicles
-              </Link>
-            </li>
-            <li className="m-0">
-              <Link
-                to="/register"
-                className="text-white font-inter font-semibold hover:text-white focus:text-white tracking-normal"
-                style={{ fontSize: "1.36rem" }}
-              >
-                Register A Vehicles
-              </Link>
-            </li>
-            <li
-              onClick={signOut}
-              className="m-0 text-white font-inter font-semibold hover:text-white focus:text-white cursor-pointer tracking-normal"
-              style={{ fontSize: "1.36rem" }}
+          <>
+            <IoMdMenu
+              onClick={() =>
+                isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
+              }
+              className={`text-5xl text-white hidden hamburger-menu cursor-pointer ${
+                isMenuOpen ? "text-blue-custom" : ""
+              }`}
+            />
+            <ul
+              className="list-none flex items-center justify-between m-0 navbar-list"
+              style={
+                isMobile
+                  ? isMenuOpen
+                    ? { display: "flex" }
+                    : { display: "none" }
+                  : { display: "flex" }
+              }
             >
-              Sign Out
-            </li>
-          </ul>
+              <li className="m-0">
+                <Link
+                  to="/"
+                  className="text-white font-inter font-semibold hover:text-white focus:text-white tracking-normal mr-20"
+                  style={{ fontSize: "1.335rem" }}
+                >
+                  Your Vehicles
+                </Link>
+              </li>
+              <li className="m-0">
+                <Link
+                  to="/register"
+                  className="text-white font-inter font-semibold hover:text-white focus:text-white tracking-normal mr-20"
+                  style={{ fontSize: "1.335rem" }}
+                >
+                  Register A Vehicles
+                </Link>
+              </li>
+              <li
+                onClick={signOut}
+                className="m-0 text-white font-inter font-semibold hover:text-white focus:text-white cursor-pointer tracking-normal"
+                style={{ fontSize: "1.335rem" }}
+              >
+                Sign Out
+              </li>
+            </ul>
+          </>
         ) : (
           ""
         )}
